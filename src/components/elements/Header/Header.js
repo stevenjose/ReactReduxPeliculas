5ff4a99384d0f5a3b1ad8e60c3aebad3adebbc8f
Jose lopez <lopezajoseg@gmail.com>
2020-06-17 13:56:39 -0400

    feature-1

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-
+import profileImage from '../../../images/reactMovie_logo.png';
 const Header = () => (
   <div className="rmdb-header">
     <div className="rmdb-header-content">
       <Link to="/">
-        <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+        <img className="rmdb-logo" src={ profileImage } alt="rmdb-logo" />
       </Link>
       <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
     </div>
