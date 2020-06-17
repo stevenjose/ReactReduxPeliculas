ebf1c949f9fe73a92c588ac64f228d22458df337
Jose lopez <lopezajoseg@gmail.com>
2020-06-17 13:25:34 -0400

    initial

---
+++ build/precache-manifest.8f60ea425471146c04221ea4ac905ddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9e786c12f92095f09c",
+    "url": "/ReactReduxPeliculas/static/css/main.067de0ac.chunk.css"
+  },
+  {
+    "revision": "bb9e786c12f92095f09c",
+    "url": "/ReactReduxPeliculas/static/js/main.bb9e786c.chunk.js"
+  },
+  {
+    "revision": "77fcc32944b6874eb862",
+    "url": "/ReactReduxPeliculas/static/js/1.77fcc329.chunk.js"
+  },
+  {
+    "revision": "32cc4e54cbcd6b14f0bf",
+    "url": "/ReactReduxPeliculas/static/js/runtime~main.32cc4e54.js"
+  },
+  {
+    "revision": "f58c4c01a4f334775b4d0cdb7a554145",
+    "url": "/ReactReduxPeliculas/index.html"
+  }
+];
